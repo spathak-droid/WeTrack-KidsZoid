e63431c8c7751855029e73aec8f37a6b57745ead
spathak-droid <pathaksandesh025@gmail.com>
2021-07-06 17:22:02 -0500

    hello

--- app/src/main/java/com/example/kidszoid/MainActivity.java
+++ app/src/main/java/com/example/kidszoid/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
-        //Animations
+        //Animation
         top = AnimationUtils.loadAnimation(this,R.anim.top_aa);
         bottom = AnimationUtils.loadAnimation(this, R.anim.buttom_aa);
         image = findViewById(R.id.gifImageView);
